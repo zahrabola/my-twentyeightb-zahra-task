116c65091ccff7f783cf8c40b12ff4f1fb7e225b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-02-24 18:35:34 +0000

    Update Css
    Styling Sidebar, rootStyles, hover grey, and font becomes bold

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,35 +1,49 @@
 import React, { useState } from "react";
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { VscBlank } from "react-icons/vsc";
+import { Sidebar, Menu, MenuItem, menuClasses } from "react-pro-sidebar";
 
 const SideNav = () => {
   const [toggled, setToggled] = useState(false);
-  
 
   return (
-    <div style={{ display: "flex", height: "100%", minHeight: "400px" }}>
+    <div style={{ display: "flex", height: "100vh", minHeight: "400px" }}>
       <Sidebar
         breakPoint="sm"
         onBackdropClick={() => setToggled(false)}
         toggled={toggled}
         backgroundColor="#333333"
-
       >
-            <Menu>
-            <MenuItem icon={<VscBlank />}></MenuItem>
-            </Menu>
-        <Menu>
-          <MenuItem disabled>Welcome</MenuItem>
+        <Menu
+          className="sidebar_menu"
+          rootStyles={{
+            ["." + menuClasses.button]: {
+              backgroundColor: "#333333",
+              color: "#1d1d1",
+              "&:hover": {
+                backgroundColor: "#696868",
+                fontWeight: "bold",
+              },
+            },
+          }}
+        >
+          <MenuItem disabled className="welcome">
+            Welcome,
+          </MenuItem>
           <MenuItem>Andy Repp</MenuItem>
-        
-          <MenuItem disabled>Metrics</MenuItem>
-          <MenuItem > Overview</MenuItem>
+
+          <MenuItem disabled className="metrics">
+            Metrics
+          </MenuItem>
+          <MenuItem className="overview" active>
+            {" "}
+            Overview
+          </MenuItem>
           <MenuItem> Detailing</MenuItem>
           <MenuItem>Calls</MenuItem>
-          
-        
-          <MenuItem disabled >Information</MenuItem>
-          <MenuItem icon={<VscBlank />}></MenuItem>
+
+          <MenuItem disabled className="information">
+            Information
+          </MenuItem>
+
           <MenuItem>About profiles</MenuItem>
           <MenuItem>Detailing targets</MenuItem>
         </Menu>
